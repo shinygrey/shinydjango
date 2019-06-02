5783c40fc363263eb54716e8903f3f61e7f486af
Greg <git@shinygrey.com>
2019-06-02 16:51:51 +0100

    added website folder

---
+++ website/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+import home.views
+import hello.views
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+    path("", home.views.index, name="index"),
+    path("db/", hello.views.db, name="db"),
+    path("admin/", admin.site.urls),
+]
