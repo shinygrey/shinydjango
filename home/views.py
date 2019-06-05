4a6c62328b1bd8640772defae1a743303c78511c
Greg <git@shinygrey.com>
2019-06-05 23:33:48 +0100

    Get request 12

--- home/views.py
+++ home/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 from .models import Experiments
 
 def index(request):
-    test1 = Experiments("4")
-	#firstName = test1.data["data"]["first_name"]	
+	test1 = Experiments("4")
+	#firstName = test1.data["data"]["first_name"]
 	
-	dictionarytest = {"data": {"first_name": "Helen"}}	
+	dictionarytest = {"data": {"first_name": "Helen"}}
 	testName = dictionarytest["data"]["first_name"]
 	currentTime = test1.datetime1
 	return render(request, "index.html", {"greetings": currentTime, "test": testName})
