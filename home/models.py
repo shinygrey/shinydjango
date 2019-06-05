3d856cfd64be1597ca7c8829ff28d16a42ec6a45
Greg <git@shinygrey.com>
2019-06-05 22:57:23 +0100

    Get request 5

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ import requests
 class Experiments():
 	def __init__(self):
 		self.response = requests.get("https://reqres.in/api/users/2")
+		self.responsedata = self.response.json()
 		
-	responsedata = response.json()
+	
 	datetime1 = datetime.datetime.now()
