e01ad9e6f4908a1478af244a3ed8529fa6c5483d
Greg <git@shinygrey.com>
2019-06-05 23:21:25 +0100

    Get request haaaannds

--- home/views.py
+++ home/views.py
@@ -6,8 +6,13 @@ from .models import Experiments
 def index(request):
 
     test1 = Experiments("4")
-	firstName = test1["data"]["first_name"]
+	firstName = test1.data["data"]["first_name"]	
+	
+	dictionarytest = {"data": {"first_name": "Helen"}}	
+	testName = dictionarytest["data"]["first_name"]
+	
+	
     currentTime = test1.datetime1
 	
 
-    return render(request, "index.html", {"greetings": currentTime, "restdata": test1.firstName})
+    return render(request, "index.html", {"greetings": currentTime, "restdata": firstName}, "test": testName)
