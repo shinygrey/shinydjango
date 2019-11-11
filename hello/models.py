6c0e413ae5a5d8ad29b7d11b1f9218fabff7c8f8
Shiny Grey ltd <42606419+shinygrey@users.noreply.github.com>
2019-11-11 19:51:59 +0000

    Update models.py

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+    
+    
+    
+    
+class Choices():
+    mychoices = ["apple", "banana", "cherry", "grape", "orange"]
